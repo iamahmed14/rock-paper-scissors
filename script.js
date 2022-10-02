ee2e1dccf79c2704fae91eb7959c53a0276c16f6
Ahmed <mohammadahmed940@gmail.com>
2022-10-03 01:29:32 +0500

    making 5 rounds game

--- script.js
+++ script.js
@@ -7,15 +7,38 @@ function getComputerChoice() {
 
 function playRound(cChoice, pChoice) {
 
-    if (cChoice=="rock" && pChoice=="paper") return "You Win, Paper beats Rock!";
-    else if (cChoice=="rock" && pChoice=="scissors") return "You Lose, Rock beats Scissors!";
-    else if (cChoice=="paper" && pChoice=="rock") return "You Lose, Paper beats Rock!";
-    else if (cChoice=="paper" && pChoice=="scissors") return "You Win, Scissors beats Paper!";
-    else if (cChoice=="scissors" && pChoice=="rock") return "You Win, Rock beats Scissors!";
-    else if (cChoice=="scissors" && pChoice=="paper") return "You Lose, Scissors beats Paper!";
-    else if (cChoice==pChoice) return "Its a tie";
+    if ((cChoice=="rock" && pChoice=="paper") ||
+    (cChoice=="paper" && pChoice=="scissors") ||
+    (cChoice=="scissors" && pChoice=="rock"))
+        return "player";
+
+    else if ((cChoice=="rock" && pChoice=="scissors") ||
+    (cChoice=="paper" && pChoice=="rock") ||
+    (cChoice=="scissors" && pChoice=="paper"))
+        return "computer";
+
+    else if (cChoice==pChoice)
+        return "tie";
 }
 
-const cChoice = getComputerChoice();
-const pChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
-console.log(playRound(cChoice, pChoice));
+function game() {
+
+    for (let i=0; i<5; i++) {
+
+        const cChoice = getComputerChoice();
+        const pChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+
+        let win = playRound(cChoice, pChoice)
+        if (win=="player") {
+            console.log("You Win, " + pChoice + " beats " + cChoice);
+
+        } else if (win=="computer") {
+            console.log("You Lose, " + cChoice + " beats " + pChoice);
+
+        } else if (win=="tie") {
+            console.log("Its a tie!");
+        }
+    }
+}
+
+game();
