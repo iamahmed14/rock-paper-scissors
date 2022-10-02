b0ad36a2534c5d57568f818b3f3486a53613bedc
Ahmed <mohammadahmed940@gmail.com>
2022-10-02 16:47:25 +0500

    wrote one round game playable on console

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let cChoice = Math.floor(Math.random() * 3);
+    if (cChoice == 0) return "rock";
+    else if ( cChoice == 1) return "paper";
+    else return "scissors";
 }
-console.log(getComputerChoice());
+
+function playRound(cChoice, pChoice) {
+
+    if (cChoice=="rock" && pChoice=="paper") return "You Win, Paper beats Rock!";
+    else if (cChoice=="rock" && pChoice=="scissors") return "You Lose, Rock beats Scissors!";
+    else if (cChoice=="paper" && pChoice=="rock") return "You Lose, Paper beats Rock!";
+    else if (cChoice=="paper" && pChoice=="scissors") return "You Win, Scissors beats Paper!";
+    else if (cChoice=="scissors" && pChoice=="rock") return "You Win, Rock beats Scissors!";
+    else if (cChoice=="scissors" && pChoice=="paper") return "You Lose, Scissors beats Paper!";
+    else if (cChoice==pChoice) return "Its a tie";
+}
+
+const cChoice = getComputerChoice();
+const pChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+console.log(playRound(cChoice, pChoice));
